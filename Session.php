b7f2d33679ac6896315547d4cf76cc2086a8b811
Sarah Saleh <saraay432@gmail.com>
2024-02-13 19:11:00 +0300

    commit

---
+++ Session.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace app\core;
+
+class Session 
+{
+    protected const FALSH_KEY = 'flash_messages';
+
+    public function __construct(){
+        session_start();
+        $flashMessages = $_SESSION[self::FALSH_KEY]?? [];
+        foreach($flashMessages as $key =>&$flashMessage){
+            $flashMessage['remove'] = true;
+        }
+        $_SESSION[self::FALSH_KEY] = $flashMessages;
+    } 
+
+    public function setFlash($key, $message)
+    {
+        $_SESSION[self::FALSH_KEY][$key] = [
+            'remove' => false,
+            'value' => $message
+        ];
+    }
+    public function getFlash($key)
+    {
+        return $_SESSION[self::FALSH_KEY][$key]['value'] ?? false;
+    }
+    public function set($key,$value){
+        $_SESSION[$key] = $value;
+    }
+    public function get($key){
+        return $_SESSION[$key] ?? false;
+    }
+    public function remove($key){
+        unset($_SESSION[$key]);
+    }
+    public function __destruct()
+    {
+        $flashMessages = $_SESSION[self::FALSH_KEY]?? [];
+        foreach($flashMessages as $key =>&$flashMessage){
+            if($flashMessage['remove']){
+                unset($flashMessages[$key]);
+            }
+        }
+        $_SESSION[self::FALSH_KEY] = $flashMessages;
+    }
+}
---
+++ form/TextareaField.php
@@ -0,0 +1,21 @@
+<?php 
+
+namespace app\core\form;
+use app\core\form\BaseField;
+use app\core\Model;
+
+
+class TextareaField extends BaseField
+{   
+
+    public function renderInput(): string {
+        return sprintf('<textarea name="%s" class="form-control%s">%s</textarea>',
+        $this->attribute,
+        $this->model->hasError($this->attribute) ? ' is-invalid' : '',
+        $this->model->{$this->attribute},
+       );
+        
+    }
+
+
+}
